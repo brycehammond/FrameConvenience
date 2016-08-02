835838b71ca73e7e9f1ed9c3aa7e2d538fb91b0c
Bryce Hammond <bryce@flyreel.co>
2016-08-02 15:53:45 -0600

    Add frame convenience extensions

---
+++ FrameConvenience.swift
@@ -0,0 +1,136 @@
+import UIKit
+
+extension UIView {
+    
+    var frameOrigin: CGPoint {
+        get {
+            return self.frame.origin
+        }
+        set(newValue) {
+            var viewFrame = self.frame
+            viewFrame.origin = newValue
+            self.frame = viewFrame
+            
+        }
+    }
+    
+    var frameXOrigin: CGFloat {
+        get {
+            return self.frame.origin.x
+        }
+        set(newValue) {
+            var viewFrame = self.frame
+            viewFrame.origin.x = newValue
+            self.frame = viewFrame
+        }
+    }
+    
+    var frameYOrigin: CGFloat {
+        get {
+            return self.frame.origin.y
+        }
+        set(newValue) {
+            var viewFrame = self.frame
+            viewFrame.origin.y = newValue
+            self.frame = viewFrame
+        }
+    }
+    
+    var frameSize: CGSize {
+        get {
+            return self.frame.size
+        }
+        set(newValue) {
+            var viewFrame = self.frame
+            viewFrame.size = newValue
+            self.frame = viewFrame
+        }
+    }
+    
+    var frameHeight: CGFloat {
+        get {
+            return self.frame.size.height
+        }
+        set(newValue) {
+            var viewFrame = self.frame
+            viewFrame.size.height = newValue
+            self.frame = viewFrame
+        }
+    }
+    
+    var frameWidth: CGFloat {
+        get {
+            return self.frame.size.width
+        }
+        set(newValue) {
+            var viewFrame = self.frame
+            viewFrame.size.width = newValue
+            self.frame = viewFrame
+        }
+    }
+    
+    var rightBorderXValue: CGFloat {
+        get {
+            return CGRectGetMaxX(self.frame)
+        }
+        set(newValue) {
+            var viewFrame = self.frame
+            viewFrame.origin.x = newValue - viewFrame.size.width
+            self.frame = viewFrame
+        }
+    }
+    
+    var bottomBorderYValue: CGFloat {
+        get {
+            return CGRectGetMaxY(self.frame)
+        }
+        set(newValue) {
+            var viewFrame = self.frame
+            viewFrame.origin.y = newValue - viewFrame.size.height
+            self.frame = viewFrame
+        }
+    }
+    
+    func frameForBorderWithSize(size: CGFloat) -> CGRect {
+        let x = self.frameXOrigin - size
+        let y = self.frameYOrigin - size
+        let width = self.frameWidth + (size * 2)
+        let height = self.frameHeight + (size * 2)
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+    
+    func centerVerticallyInSuperviewWithXOrigin(xOrigin: CGFloat) {
+        if let superview = self.superview {
+            let superviewBounds = superview.bounds
+            var viewFrame = self.frame
+            viewFrame.origin.x = xOrigin
+            viewFrame.origin.y = floor((superviewBounds.size.height - viewFrame.size.height) / 2)
+            self.frame = viewFrame
+        }
+    }
+    
+    func centerHorizontallyInSuperviewWithYOrigin(yOrigin: CGFloat) {
+        if let superview = self.superview {
+            let superviewBounds = superview.bounds
+            var viewFrame = self.frame
+            viewFrame.origin.y = yOrigin
+            viewFrame.origin.x = floor((superviewBounds.size.width - viewFrame.size.width) / 2)
+            self.frame = viewFrame
+        }
+    }
+    
+    func centerInSuperview() {
+        self.centerInSuperviewWithOffset(CGPointZero)
+    }
+    
+    func centerInSuperviewWithOffset(offset: CGPoint) {
+        if let superview = self.superview {
+            let superviewBounds = superview.bounds
+            var viewFrame = self.frame
+            viewFrame.origin.x = floor((superviewBounds.size.width - viewFrame.size.width) / 2) + offset.x
+            viewFrame.origin.y = floor((superviewBounds.size.height - viewFrame.size.height) / 2) + offset.y
+            self.frame = viewFrame
+        }
+    }
+    
+}
